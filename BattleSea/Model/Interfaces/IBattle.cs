f23040b6ecb930ab44fdc953a10218f45fdea509
MashiroJiro <spbfive1@gmail.com>
2022-01-09 15:34:45 +0300

    Добавление интерфейсов

---
+++ BattleSea/Model/Interfaces/IBattle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BattleSea.Model.Interfaces
+{
+    interface IBattle
+    {
+        bool Shoot(Point point, IBattle enemy);
+        Ship CreateShip(Point start);
+        bool IsAllShipAdded();
+        bool IsAllShipDead();
+        List<Ship> GetShips();
+    }
+}
---
+++ BattleSea/Service/Interfaces/IShootService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleSea.Model;
+
+namespace BattleSea.Service.Interfaces
+{
+    interface IShootService
+    {
+        bool Shoot(Point point);
+        bool IsAllShipDead();
+
+    }
+}
