7879ef600e383dacae5941632326ee8b145341d4
MashiroJiro <spbfive1@gmail.com>
2022-01-09 15:37:30 +0300

    Добавление тестов для моделей

---
+++ BattleSea/Model/Tests/BattleTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using BattleSea.Model;
+
+namespace BattleSea.Model.Test
+{
+    [TestFixture]
+    class BattleTest
+    {
+        public Battle battle = Battle.GenerateRandom();
+
+        [SetUp]
+        public void SetUp()
+        {
+            foreach (var ship in battle.GetShips())
+            {
+                ship.GetPoint().Hit = true;               
+            }
+        }
+
+        [Test]
+        public void IsAllShipKilledTest()
+        {
+            Assert.IsTrue(battle.IsAllShipDead());
+        }
+
+        [Test]
+        public void IsNotAllShipKIlledTest()
+        {
+            battle.GetShips()[0].GetPoint().Hit = false;
+            Assert.IsFalse(battle.IsAllShipDead());
+        }
+    }
+}
